cc12c1738b92fa7480b8b5136944be5b7cf5c2b0
Julia Kim <juliaykim6@gmail.com>
2021-04-05 20:39:53 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
 INSERT INTO bears(name, age, gender, color, temperament, alive) 
 VALUES ("Mr. Chocolate", 2, "male", "brown", "g", 1);
 INSERT INTO bears(name, age, gender, color, temperament, alive) 
-VALUES (2, "Rowdy", 2, "female", "tan", "g", 1;
+VALUES ("Rowdy", 2, "female", "tan", "g", 1;
 INSERT INTO bears(name, age, gender, color, temperament, alive)
-VALUES (3, "Tabitha", 10, "female", "white", "g", 0);
+VALUES ("Tabitha", 10, "female", "white", "g", 0);
 INSERT INTO bears(name, age, gender, color, temperament, alive) 
-VALUES (4, "Sergeant Brown", 5, "male", "gray", "q", 1);
+VALUES ("Sergeant Brown", 5, "male", "gray", "q", 1);
 INSERT INTO bears(name, age, gender, color, temperament, alive) 
-VALUES (5, "Melissa", 3, "female", "white", "q", 1);
+VALUES ("Melissa", 3, "female", "white", "q", 1);
 INSERT INTO bears(name, age, gender, color, temperament, alive)
-VALUES (6, "Grinch", 2, "male", "gray", "q", 1);
+VALUES ("Grinch", 2, "male", "gray", "q", 1);
 INSERT INTO bears(name, age, gender, color, temperament, alive) 
-VALUES (7, "Wendy", 7, "female", "black", "g", 0);
+VALUES ("Wendy", 7, "female", "black", "g", 0);
 INSERT INTO bears(name, age, gender, color, temperament, alive) 
-VALUES (8, NULL, 3, "male", "black", "q", 1);
+VALUES (NULL, 3, "male", "black", "q", 1);
