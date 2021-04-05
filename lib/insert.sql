47beacbdc761661ab996f46c52e81cbc831432ec
Julia Kim <juliaykim6@gmail.com>
2021-04-05 20:44:25 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
-insert into bears(id, name, age, gender, color, temperament, alive) 
-values (1, "Mr. Chocolate", 2, "male", "brown", "g", 1);
-insert into bears(name, age, gender, color, temperament, alive) 
-values (2, "Rowdy", 2, "female", "tan", "g", 1;
-insert into bears(name, age, gender, color, temperament, alive)
-values (3, "Tabitha", 10, "female", "white", "g", 0);
-insert into bears(name, age, gender, color, temperament, alive) 
-values (4, "Sergeant Brown", 5, "male", "gray", "q", 1);
-insert into bears(name, age, gender, color, temperament, alive) 
-values (5, "Melissa", 3, "female", "white", "q", 1);
-insert into bears(name, age, gender, color, temperament, alive)
-values (6, "Grinch", 2, "male", "gray", "q", 1);
-insert into bears(name, age, gender, color, temperament, alive) 
-values (7, "Wendy", 7, "female", "black", "g", 0);
-insert into bears(name, age, gender, color, temperament, alive) 
-values (8, NULL, 3, "male", "black", "q", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) 
+VALUES ("Mr. Chocolate", 2, "male", "brown", "g", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) 
+VALUES "Rowdy", 2, "female", "tan", "g", 1;
+INSERT INTO bears(name, age, gender, color, temperament, alive)
+VALUES ("Tabitha", 10, "female", "white", "g", 0);
+INSERT INTO bears(name, age, gender, color, temperament, alive) 
+VALUES ("Sergeant Brown", 5, "male", "gray", "q", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) 
+VALUES ("Melissa", 3, "female", "white", "q", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive)
+VALUES ("Grinch", 2, "male", "gray", "q", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) 
+VALUES ("Wendy", 7, "female", "black", "g", 0);
+INSERT INTO bears(name, age, gender, color, temperament, alive) 
+VALUES (NULL, 3, "male", "black", "q", 1);
