9e0edfa3fcc4d5b2688f67bf00302bafe44bbbdd
Julia Kim <juliaykim6@gmail.com>
2021-04-05 20:51:59 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,10 @@
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 2, 'male', 'brown', 'g', true);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Rowdy', 2, 'female', 'tan', 'g', false);
-INSERT INTO bears(name, age, gender, color, temperament, alive)VALUES ('Tabitha', 10, 'female', 'white', 'g', false);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 5, 'male', 'gray', 'q', true);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Melissa', 3, 'female', 'white', 'q', true);
-INSERT INTO bears(name, age, gender, color, temperament, alive)VALUES ('Grinch', 2, 'male', 'gray', 'q', true);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ('Wendy', 7, 'female', 'black', 'g', false);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (NULL, 3, 'male', 'black', 'q', true);
+
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "Male", "Brown", "Nice", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 5, "Male", "Black", "Mean", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 18, "Female", "White", "Nice", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 35, "Male", "Brown", "Mean", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 2, "Female", "Black & Brown", "Nice", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 42, "Male", "Green", "Mean", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 12, "Female", "Auburn", "Nice", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 20, "Male", "Brown", "Nice", 0);
