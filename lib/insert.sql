6118ba627c816fddba36ef2ed1e0c5c141c27d67
Julia Kim <juliaykim6@gmail.com>
2021-04-05 20:31:57 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,2 @@
-INSERT INTO bears(
-name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, male, brown, g, yes)
+INSERT INTO bears(name, age, gender, color, temperament, alive) 
+VALUES ("Mr. Chocolate", 2, male, brown, g, yes);
