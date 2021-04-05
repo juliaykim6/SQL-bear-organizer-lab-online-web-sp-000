38d04a3be3575547d63ed02fb3dfa25a70dae8f9
Julia Kim <juliaykim6@gmail.com>
2021-04-05 20:38:24 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,16 @@
 INSERT INTO bears(name, age, gender, color, temperament, alive) 
-VALUES (1, "Mr. Chocolate", 2, "male", "brown", "g", 0);
+VALUES (1, "Mr. Chocolate", 2, "male", "brown", "g", 1);
 INSERT INTO bears(name, age, gender, color, temperament, alive) 
-VALUES (2, "Rowdy", 2, "female", "tan", "g", 0);
-
+VALUES (2, "Rowdy", 2, "female", "tan", "g", 1;
+INSERT INTO bears(name, age, gender, color, temperament, alive)
+VALUES (3, "Tabitha", 10, "female", "white", "g", 0);
+INSERT INTO bears(name, age, gender, color, temperament, alive) 
+VALUES (4, "Sergeant Brown", 5, "male", "gray", "q", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) 
+VALUES (5, "Melissa", 3, "female", "white", "q", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive)
+VALUES (6, "Grinch", 2, "male", "gray", "q", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) 
+VALUES (7, "Wendy", 7, "female", "black", "g", 0);
+INSERT INTO bears(name, age, gender, color, temperament, alive) 
+VALUES (8, "Sergeant Brown", 5, "male", "gray", "q", 1);
